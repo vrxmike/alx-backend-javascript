69f7fef8fa5c036974ea2a73cd88ceae8c679034
vrxmike <alvinvrxmike@icloud.com>
2023-06-27 13:10:33 +0000

    2-Task-Arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', Union Square'];
+
+  const self = this;
+  this.addNeighborhoods.push(newNeighborhood);
+    self.sanFranciscoNeighborhoods.push(newNeighborhoods);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
