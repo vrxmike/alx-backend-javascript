b924b35b1895d4b5fa9a74230fd6d73ae6ab19d6
vrxmike <alvinvrxmike@icloud.com>
2023-08-19 23:26:58 +0000

    fix 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-export default function getFullResponseFromAPI(sucess) {
+export default function getFullResponseFromAPI(success) {
   const arr = new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
