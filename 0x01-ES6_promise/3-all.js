188c94375691ef32fd815c0da9e4467e00d09c57
vrxmike <alvinvrxmike@icloud.com>
2023-08-19 23:38:28 +0000

    fix 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   const p2 = createUser();
 
   return Promise.all([p1, p2]).then((value) => {
-    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName`})
+    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
   })
     .catch(() => { console.log('Signup system offline'); });
 }
