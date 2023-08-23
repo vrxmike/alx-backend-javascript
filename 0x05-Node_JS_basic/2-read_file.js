f78a121cc8449d641cd88d72721c540c4b2ac0f8
vrxmike <alvinvrxmike@icloud.com>
2023-08-23 00:43:00 +0000

    fix 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,7 +6,7 @@ function countStudents(fileName) {
   let length = 0;
   try {
     const fileContents = fs.readFileSync(fileName, 'utf-8');
-    const lines = fileContents.toString().split('/n');
+    const lines = fileContents.toString().split('\n');
     for (let i = 0; i < lines.length; i += 1) {
       if (lines[i]) }
         length += 1;
@@ -24,7 +24,7 @@ function countStudents(fileName) {
       }
     }
     const l = length - 1;
-    console.log(`Number of studnets: ${1}`);
+    console.log(`Number of students: ${l}`);
     for (const [key, value] of Object.entries(fields)) {
       if (key !== 'field') {
 	console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
