7b49969983dead336d7fd4cb4b3dd91835973440
vrxmike <alvinvrxmike@icloud.com>
2023-08-24 02:14:53 +0000

    fix 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const ( it, describe } = require("mocha");
+const { it, describe } = require("mocha");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber()", function() {
