1ffd7cdbab58a63dca19b89bd2e27dfbdc8dc726
vrxmike <alvinvrxmike@icloud.com>
2023-08-24 21:40:33 +0000

    update all added files

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,8 @@
+const utils = require("./utils");
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
