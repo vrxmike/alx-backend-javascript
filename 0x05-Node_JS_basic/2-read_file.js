6b8e5daddc5c4fcdb3a50d8dcbd3e9c806209b42
vrxmike <alvinvrxmike@icloud.com>
2023-08-24 01:00:10 +0000

    fix lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -11,23 +11,23 @@ function countStudents(fileName) {
       if (lines[i]) {
         length += 1;
         const field = lines[i].toString().split(',');
-	if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-	  students[field[3]].push(field[0]);
-	} else {
-	  students[field[3]] = [field[0]];
-	}
-	if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-	  fields[field[3]] += 1;
-	} else {
-	  fields[field[3]] = 1;
-	}
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
       }
     }
     const l = length - 1;
     console.log(`Number of students: ${l}`);
     for (const [key, value] of Object.entries(fields)) {
       if (key !== 'field') {
-	console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
       }
     }
   } catch (error) {
