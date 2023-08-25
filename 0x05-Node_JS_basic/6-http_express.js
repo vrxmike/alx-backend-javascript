f96b319999a49fde32f7ebcc0b9abbfeb43d5d85
vrxmike <alvinvrxmike@icloud.com>
2023-08-25 20:22:01 +0000

    hot fix 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 1245;
 
 app.get('/', (request, response) => {
-  response.send('Hello Holberton School!);
+  response.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
