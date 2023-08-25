3f68b7a20ecd2c7b799fff4a51cb85834a71d9bc
vrxmike <alvinvrxmike@icloud.com>
2023-08-25 20:12:01 +0000

    hot fix 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const { readfile } = require('fs');
+const { readFile } = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -11,35 +11,35 @@ function countStudents(fileName) {
   return new Promise((resolve, reject) => {
     readFile(fileName, (err, data) => {
       if (err) {
-        rejeect(err);
+        reject(err);
       } else {
-	let output = '';
-	const lines = data.toString().split('\n');
-	for (let i = 0; i < lines.length; i += 1) {
-	  if (lines[i]) {
-	    length += 1;
-	    const field = lines[i].toString().split(',');
-	    if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-	      students[field[3]].push(field[0]);
-	    } else {
-	      students[field[3]] = [field[0]];
-	    }
-	    if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-	      fields[field[3]] += 1;
-	    } else {
-	      fields[field[3]] = 1;
-	    }
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
           }
         }
-	const l = length - 1;
-	output += `Number of students: ${l}\n`;
-	for (const [key, value] of Object.entries(fields)) {
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
           if (key !== 'field') {
             output += `Number of students in ${key}: ${value}. `;
-	    output += `List: ${students[key].join(', ')}\n`;
-	  }
-	}
-	resolve(output);
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
       }
     });
   });
