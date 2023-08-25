f81b691697472eaff51ce726cb20f0e94f244332
vrxmike <alvinvrxmike@icloud.com>
2023-08-25 20:33:56 +0000

    update fix 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -15,31 +15,31 @@ function countStudents(fileName) {
         reject(err);
       } else {
         let output = '';
-	const lines = data.toString().split(', ');
-	for (let i = 0; i < lines.length; i += 1) {
-	  if (lines[i]) {
-	    length += 1;
-	    const field = lines[i].toString().split(', ');
-	    if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-	      students[field[3]].push(field[0]);
-	    } else {
-	      students[field[3]] = [field[0]];
-	    }
-	    if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-	      fields[field[3]] += 1;
-	    } else {
-	      fields[field[3]] = 1;
-	    }
-	  }
-	}
-	const l = length - 1;
-	output += `Number of students: ${l}\n`;
-	for (const [key, value] of Object.entries(fields)) {
-	  if (key !== 'field') {
-	    output += `Number of students in ${key}: ${value}. `;
-	    output += `List: ${students[key].join(', ')}\n`;
-	  }
-	}
+        const lines = data.toString().split(', ');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(', ');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
         resolve(output);
       }
     });
@@ -60,4 +60,4 @@ app.get('/students', (request, response) => {
 app.listen(port, () => {
 });
 
-module.exports = app
+module.exports = app;
