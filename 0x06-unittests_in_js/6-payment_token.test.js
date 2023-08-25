e6d24b227e358299f7bf78ae31c685893a829d26
vrxmike <alvinvrxmike@icloud.com>
2023-08-25 01:23:08 +0000

    fix 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ const expect = require("chai").expect;
 describe("getPaymentTokenFromAPI", function() {
     it("Async testing with done callback", function(done) {
         getPaymentTokenFromAPI(true)
-             .then((data) +> {
+             .then((data) => {
                  expect(data).to.have.property('data');
                  done();
              });
