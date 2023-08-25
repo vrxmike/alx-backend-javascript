558323cdda2ae127095908fb78e0d58be6784f83
vrxmike <alvinvrxmike@icloud.com>
2023-08-25 01:14:37 +0000

    update fix for 3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require("./utils");
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
     const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
-    console.log(`The total is: ${result}}`);
+    console.log(`The total is: ${result}`);
 }
 
 module.exports = sendPaymentRequestToApi;
