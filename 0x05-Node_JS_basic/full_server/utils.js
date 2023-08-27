e78cef592d6cc9aaef609d7607584878a60bd540
vrxmike <alvinvrxmike@icloud.com>
2023-08-27 23:30:56 +0000

    fix utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -5,21 +5,21 @@ module.exports = function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     readFile(filePath, (err, data) => {
       if (err) {
-	reject(err);
+        reject(err);
       } else {
-	const lines = data.toString().split('\n');
-	const noHeader = lines.slice(1);
-	for (let i = 0; i < noHeader.length; i += 1) {
+        const lines = data.toString().split('\n');
+        const noHeader = lines.slice(1);
+        for (let i = 0; i < noHeader.length; i += 1) {
           if (noHeader[i]) {
-	    const field = noHeader[i].toString().split(', ');
-	    if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-	      students[field[3]].push(field[0]);
-	    } else {
-	      students[field[3]] = [field[0]];
-	    }
+            const field = noHeader[i].toString().split(', ');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
           }
-	}
-	resolve(students);
+        }
+        resolve(students);
       }
     });
   });
