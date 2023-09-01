51e1bda75d71649378321319fde1755747e038e7
vrxmike <alvinvrxmike@icloud.com>
2023-09-01 21:31:59 +0000

    fix 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from './0-classroom';
 
-export default function initializationRooms() {
+export default function initializeRooms() {
   const arr = [
     new ClassRoom(19),
     new ClassRoom(20),
