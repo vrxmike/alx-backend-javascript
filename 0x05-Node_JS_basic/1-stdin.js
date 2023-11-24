173d8619064d8029d9878d194a44089a799e4409
vrxmike <alvinvrxmike@icloud.com>
2023-11-24 23:12:15 +0000

    update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: $(data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 } else {
