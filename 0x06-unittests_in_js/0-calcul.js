b7dfb63bf667147e8741a9e4071893fb3138d243
vrxmike <alvinvrxmike@icloud.com>
2024-01-26 20:26:38 +0300

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/0-calcul.js
+++
@@ -1,7 +0,0 @@
-function calculateNumber(a, b) {
-    const an = Math.round(a);
-    const bn = Math.round(b);
-    return c;
-}
-
-module.exports = calculateNumber;
--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,9 +0,0 @@
-function getPaymentTokenFromAPI(success) {
-    if (success) {
-        return new Promise((resolve) => {
-
-        })
-    }
-}
-
-module.exports = getPaymentTokenFromAPI;
