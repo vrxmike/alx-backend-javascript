4c7e4b6b5aaaa3046b3e83570fe9c8610244041f
vrxmike <alvinvrxmike@icloud.com>
2024-02-01 14:07:39 +0000

    fix task-1

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,4 +1,4 @@
-function calculateNUmber(type, a, b) {
+function calculateNumber(type, a, b) {
     const an = Math.round(a);
     const bn = Math.round(b);
     let c = 0;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,16 +1,20 @@
 const assert = require("assert");
 const { it, describe } = require("mocha");
-const calculateNUmber = require("./1-calcul");
+const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber()", function() {
 
     it(`checking if numbers round`, function() {
       const res = calculateNumber("SUM", 1, 2);
-      assert.strictEqual(res, );
+      assert.strictEqual(res, 3);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("SUBTRACT", 1.4, 2.2);
       assert.strictEqual(res, -1);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber("SUBTRACT", 4.9, 2.7);
+      assert.strictEqual(res, 2);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 4, 2);
@@ -18,10 +22,6 @@ describe("calculateNumber()", function() {
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 1.7, 0);
-      assert.strictEqual(res, "Error");
-    });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("DVIDE", 1.7, 0);
       assert.strictEqual(res, "Error");
     });
     it(`checking if numbers round`, function() {
